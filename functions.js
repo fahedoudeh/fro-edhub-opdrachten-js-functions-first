13b9c05a41038e01501c6e65c0b21aea3a5c8dcf
fahedoudeh <fahed.oudeh@gmail.com>
2025-06-24 15:25:43 +0200

    eerst opdracht uitwerkingen

--- functions.js
+++ functions.js
@@ -13,21 +13,46 @@ console.log(greeting);
 /* Opdracht 1 */
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool"  teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
+function getSchool() {
+  return "Novi Hogeschool";
+}
+const school = getSchool();
 
-
+console.log(school)
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
+function compliment() {
+  return "Lekker bezig met die functies, Fahed!";
+}
+const sweetCopliment = compliment();
+
+console.log(sweetCopliment)
 
 
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
+function getGrades() {
+   let cijfers = [8, 8.5, 6, 7]
+  return cijfers;
+}
 
+console.log(getGrades())
 
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+function getDetails() {
+  let details = {
+    firstName: 'Fahed',
+    lastName: 'Oudeh'
+  };
+  return details;
+}
+
+console.log(getDetails())
